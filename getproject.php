8ee74cd0b92f0dface69754d62bf823e3c26f32f
Saurabh Pal <palsaurabh63@gmail.com>
2019-08-11 16:15:54 +0530

    Add files via upload

---
+++ getproject.php
@@ -0,0 +1,31 @@
+<?php
+
+$conn=mysqli_connect("localhost","root","","pro");
+
+if(isset($_GET['key'])){
+	$key=$_GET['key'];
+	
+
+	$query="SELECT * FROM `protable` WHERE Match(title) Against('%$key%') OR Match(descp) Against('%$key%')" ; 
+	
+	$result=mysqli_query($conn,$query);	
+	
+		$response =array();
+		while($row=mysqli_fetch_assoc($result)){
+		array_push($response,
+		array(
+			'id'=>$row['id'],
+			'title'=>$row['title'],
+			'descp'=>$row['descp'],
+			'time'=>$row['time'],
+			'user_id'=>$row['user_id'])	);	
+						
+			}	
+			echo json_encode($response);
+}			
+		
+	
+
+mysqli_close($conn);
+
+?>
